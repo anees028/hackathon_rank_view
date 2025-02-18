6a2245a8a8abc584285bd5abe71f1ebf3a5beaa9
anees50hertz <anees50hertz@gmail.com>
2025-02-18 14:05:01 +0100

    Css applied and position fixed.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Card = ({ item }) => {
   return (
     <div className={`item-card ${item.isFixed ? "fixed-card" : ""}`}>
-      <div className="card-header">
+      <div className="card-number">
+        {item.id}
+      </div>
+      <div className="card-header" style={{flexGrow: '1'}}>
         <span className="card-title">{item.name}</span>
         <span className="card-info">{item.icon} {item.extraInfo}</span>
       </div>
