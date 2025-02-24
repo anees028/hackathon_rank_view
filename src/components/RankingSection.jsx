24fdfc1f82e6cc145bb1641a41badf17252a90da
anees50hertz <anees50hertz@gmail.com>
2025-02-24 10:26:06 +0100

    Svg's added.

--- src/components/RankingSection.jsx
+++ src/components/RankingSection.jsx
@@ -8,7 +8,7 @@ const RankingSection = ({ title, category }) => {
   return (
     <div className={`${category.fontColor} ${title === 'Asset Temperature' ? "ranking-section-temperature" : "ranking-section-health"} `}>
       <h2 className="section-title">
-        <img src={ title === 'Asset Temperature' ? `./temperature_colored.svg` : `./frame_colored.svg` } alt="icon" className="section-icon" /> {title}
+        <img src={ title === 'Asset Temperature' ? `./temperature_colored.svg` : `./frame_colored.svg` } alt="icon" className="section-icon" /> {title+" ranking"}
       </h2>
       <div className="teams-container">
         {category.items.map((item, index) => (
